b9d9696cdfbf8be7441192776b9f3624101f9f97
Alois Sailer <alois_sailer@yahoo.com.br>
2024-02-06 20:16:54 -0300

    começado aula 2

--- src/app.tsx
+++ src/app.tsx
@@ -16,10 +16,8 @@ export function App() {
       <div className="h-px bg-slate-700" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard date={new Date(2024, 6, 2)} content="Hello World" />
+        <NoteCard date={new Date()} content="Testando segundo card" />
       </div>
     </div>
   )
