6e2b2ab8c93597ec1f20494ed7e43329a144e69b
Alois Sailer <alois_sailer@yahoo.com.br>
2024-02-06 20:23:16 -0300

    com const

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,12 @@ import logo from "./assets/logo-nlw-expert.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
+const note = {
+  date: new Date(),
+  content:
+    "Grave uma nota em áudio que será convertida para texto automaticamente",
+}
+
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -16,8 +22,7 @@ export function App() {
       <div className="h-px bg-slate-700" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard date={new Date(2024, 6, 2)} content="Hello World" />
-        <NoteCard date={new Date()} content="Testando segundo card" />
+        <NoteCard note={note} />
       </div>
     </div>
   )
